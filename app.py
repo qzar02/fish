51dc8b98381ff227055e1613f54a38c92e2e8f5a
Leandro Soares <qzar02@gmail.com>
2023-11-01 23:53:56 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,36 @@
+from datetime import date
+
+import yaml
+
+from flask import Flask, abort, render_template
+
+app = Flask(__name__)
+blog = yaml.safe_load(open("blog.yaml").read())
+publish = yaml.safe_load(open("posts/publish.yaml").read())
+draft = yaml.safe_load(open("posts/publish.yaml").read())
+
+
+def get_context(path):
+    post = [
+        post
+        for post in (publish + draft + blog["pages"])
+        if post["path"] == path
+    ]
+
+    post = post[0] if post else None
+    context = {"blog": blog, "today": date.today, "post": post, "publish": publish}
+    return context
+
+
+@app.route("/<path:path>")
+def page(path):
+    context = get_context(path)
+    post = context["post"]
+    if post is None:
+        abort(404)
+    return render_template(post["template"], **context)
+
+
+if __name__ == '__main__':
+    app.config['DEBUG'] = True
+    app.run(port=5432)
---
+++ build_static.py
@@ -0,0 +1,45 @@
+import os
+import shutil
+
+import yaml
+
+from app import app
+
+
+def validate_pages(pages):
+    paths = []
+
+    for page in pages:
+
+        if page["path"] in paths:
+            raise Exception(f"Page `{page['path']}` is not unique !")
+        else:
+            paths.append(page["path"])
+
+        if os.path.exists(f"./build/{page['path']}"):
+            raise Exception(f"File {page['path']} already exists.")
+
+
+def build(pages):
+    cli = app.test_client()
+    for page in pages:
+        r = cli.get(f"/{page['path']}")
+        with open(f"./build/{page['path']}", "w") as f:
+            f.write(r.data.decode())
+    shutil.copytree("./static", "./build/static", dirs_exist_ok=True)
+
+
+def main():
+    with open("./blog.yaml") as fblog:
+        blog = yaml.safe_load(fblog.read())
+
+    with open("./posts/publish.yaml") as fpublish:
+        posts = yaml.safe_load(fpublish.read())
+
+    pages = blog["pages"] + posts
+    validate_pages(pages)
+    build(pages)
+
+
+if __name__ == '__main__':
+    main()
